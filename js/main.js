79b10226637090c51f30a2988a25155d5370188b
Oleksandr Trushyn <trushyn.aleksandr@gmail.com>
2021-03-01 01:11:25 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function () {
-    
+    $('.slider_inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false,
+    });
 });
