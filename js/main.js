170c88a078732b4904ad82edc6a28239031f398b
Oleksandr Trushyn <trushyn.aleksandr@gmail.com>
2021-03-03 01:16:53 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(function () {
-    $('.slider_inner').slick({
+    $('.slider_inner, .news_slider-inner').slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false,
     });
 
     $('select').styler();
+
+    // $('.news_slider-inner').slick({
+    //     nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    //     prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    //     infinite: false,
+    // });
+
 });
